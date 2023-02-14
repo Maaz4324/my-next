c4566adc3b0ec0f5471e612b124bad5121b65f9b
Maaz Hamed <90350901+Maaz4324@users.noreply.github.com>
2023-02-14 14:24:59 +0530

    Add files via upload

---
+++ shopping-app/src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import navbar from "@/styles/Navbar.module.css";
+
+export default function NavbarContainer() {
+  return (
+    <>
+      <div className={navbar.container}>
+        <div className={navbar.logo}>
+          <div className={navbar.imgContainer}>
+            <img src="/logo.jpeg" alt="Shri Venkateswara trading logo" />
+          </div>
+          <h2>SVT</h2>
+        </div>
+        <div className={navbar.navContact}>
+          <div className={navbar.navlinks}>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                  Navbar
+                </a>
+                <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div
+                  class="collapse navbar-collapse"
+                  id="navbarSupportedContent"
+                >
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">
+                        Home
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        Link
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+          <div className={navbar.search}>
+            <img src="/search.svg" alt="" />
+            {/* <input type="text" placeholder="search..." /> */}
+          </div>
+          <div className={navbar.number}>
+            <img src="/phone.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
